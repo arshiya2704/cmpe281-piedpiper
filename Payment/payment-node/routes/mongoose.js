572ab3c00b8af10a4b1a82e4b708585460e3d6f0
Arshiya Sethi <31984182+arshiya2704@users.noreply.github.com>
2017-10-26 03:06:00 -0700

    node set up for payment with mongoose

---
+++ Payment/payment-node/routes/mongoose.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+mongoose.connect('localhost:27017/paymentData');
+var Schema = mongoose.Schema;
+//var passportLocalMongoose = require('passport-local-mongoose');
+
+var userDataSchema = new Schema({
+    fname           : String,
+    lname           : String,
+    email           : {type : String, unique: true},
+    cardNum         : Number,
+    cardType        : String,
+    billingAddr     : String,
+    shippingAddr    : String
+});
+
+//userDataSchema.plugin(passportLocalMongoose);
+
+module.exports.UserData = mongoose.model('PaymentData', userDataSchema);
