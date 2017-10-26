be81ac012119b600a293383a24c6882fcf706921
Ashish Virani <ashishvirani10@gmail.com>
2017-10-25 22:09:22 -0700

    Added ejs rendering

---
+++ ShoppingCart/CartNode/app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var index = require('./routes/index')
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/',index.showIndex);
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
+module.exports = app;
