dcc1ac85cf20b6ab09f2e4a2959b89bca15b9f03
Manogna Sindhusha Mujje <26583601+manogna-mujje@users.noreply.github.com>
2017-11-06 23:46:24 -0800

    GET routes for Catalog Node

---
+++ Product Catalog/CatalogNode/server/models/product.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose');
+
+var product = mongoose.model('product', {
+  category: {
+    type: String,
+//    required: true
+  },
+  productId: {
+    type: Number,
+    trim: true,
+  },
+  productName: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  },
+  price: {
+    type: Number
+  },
+  stockQuantity: {
+    type: Number,
+//    required: true,
+  //  trim: true,
+    //minlength: 1
+  }
+});
+
+module.exports = {
+  product
+};
