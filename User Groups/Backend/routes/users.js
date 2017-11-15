26044bda2c208f0b6f01fbca6899e70cc94d8129
sonijigar <jigarsoni1111@gmail.com>
2017-11-14 18:14:53 -0800

    changes

--- User Groups/Backend/routes/users.js
+++ User Groups/Backend/routes/users.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var mongo = require("./mongo")
-var mongoURL = "mongodb://localhost:27017/bonappetit";
+//var mongoURL = "mongodb://localhost:27017/bonappetit";
+var mongoURL = "mongodb://ip-10-1-2-245.us-west-1.compute.internal,ip-10-1-3-35.us-west-1.compute.internal/bonapetit?replicaSet=example-replica-set"
 var router = express.Router();
-var
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send({message:"respond with a resource"});
 });
+
+router.post('/ping', function(req, res){
+    res.status(200).send({message:"Is alive"})
+
+})
 
 router.post('/creategroup', function(req, res) {
     try{
@@ -51,35 +57,4 @@ router.post('/creategroup', function(req, res) {
     }
 });
 
-app.post('/login', function(req, res) {
-    console.log('sess:', req.body);
-    mongo.connect(mongoURL, function(){
-        var coll = mongo.collection('groups')
-
-        var groupObj = {}
-        groupObj.users = req.body.users;
-        groupObj.name = req.body.name;
-        coll.insertOne(groupObj, function(err, result) {
-        if(err) {
-            res.status(500).send();
-        }
-
-        if(!user) {
-            res.status(401).send();
-        }
-        else{
-            req.session.user = user;
-            req.session.cookie.maxAge = 30 * 60 * 1000;
-            console.log('sessionssss:::', req.session);
-            console.log("session initilized");
-            var obj = user;
-            obj.stat = "logged in";
-            console.log(obj);
-            return res.status(201).json(obj);
-        }
-
-    })(req, res);
-});
-
-
-module.exports = router;
+module.exports = router
